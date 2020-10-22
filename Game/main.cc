f073d2214e2abbdcb7630319408e84ccfe736262
Miikka Mensio <miikka.mensio@tuni.fi>
2020-10-22 19:56:21 +0300

    Incluudattu tarvittavia Qt-Kirjastoja, luotu basicProjectile- sekä Spaceship-oliot ja käynnistetään varsinainen peli-ikkuna.

--- Game/main.cc
+++ Game/main.cc
@@ -1,9 +1,29 @@
+#include "spaceship.h"
+#include "initGame.h"
+
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsRectItem>
+#include <QScreen>
+/*
+//checking available screen size
+QScreen* screen = QGuiApplication::primaryScreen();
+QRect  screenGeometry = screen->geometry();
+int height = screenGeometry.height();
+int width = screenGeometry.width();
+
+*/
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Q_INIT_RESOURCE(offlinedata);
+    initGame * game = new initGame();
+
+    game->show();
+
 
     return a.exec();
 }
+
