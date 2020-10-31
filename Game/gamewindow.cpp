9306ee3320e0deeedfe961273606ff283fa70fe1
Miikka Mensio <miikka.mensio@tuni.fi>
2020-10-31 17:52:19 +0200

    changed screen dimensions struct into more user friendly int attribute in GameWindow class

--- Game/gamewindow.cpp
+++ Game/gamewindow.cpp
@@ -40,13 +40,13 @@ GameWindow::GameWindow(QWidget *parent) :
     ui->graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setLCDStyle();
 
-    dimensions size;
+
 
     _scene = new QGraphicsScene();
 
     setPicture(bkgndBig);
     ui->graphicsView->setScene(_scene);
-    _scene->setSceneRect(0,0, size.width, size.height);
+    _scene->setSceneRect(0,0, screenWidth, screenHeight);
 
     mainTimer = new QTimer(this);
     connect(mainTimer, &QTimer::timeout, _scene, &QGraphicsScene::advance);
@@ -62,7 +62,7 @@ GameWindow::GameWindow(QWidget *parent) :
 
     _player = new Player();
 
-    _player->setPos(size.width / 2, size.height - 100);
+    _player->setPos(screenWidth / 2, screenHeight - 100);
     _player->setFlag(QGraphicsItem::ItemIsFocusable);
     _player->setFocus();
     _scene->addItem(_player);
@@ -84,10 +84,10 @@ void GameWindow::resizeEvent(QResizeEvent *event)
 
         int newWidth = event->size().width();
         int newHeight = event->size().height();
-        dimensions size;
 
-        size.width = newWidth;
-        size.height = newHeight;
+
+        screenWidth = newWidth;
+        screenHeight = newHeight;
         //player_->screenWidth_ = newWidth;
         //player_->screenHeight_ = newHeight;
 
